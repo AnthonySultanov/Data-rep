cbc3a297ba86bfd492bc21069b9e0938f69415ae
AnthonySultanov <114832556+AnthonySultanov@users.noreply.github.com>
2023-12-08 11:43:45 +0000

    Update edit.js

--- lab9/src/components/edit.js
+++ lab9/src/components/edit.js
@@ -5,23 +5,18 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 export function Edit(props)
 {
-    // The useParams hook returns an object of key/value pairs of
-    // the dynamic params from the current URL that were matched by
-    //the <Route path>.
+    
     let { id } = useParams();
-    // update arrays using the React useState()
-    // and without the Array object's push() method
+    
     const [title, setTitle] = useState("");
     const [cover, setCover] = useState("");
     const [author, setAuthor] = useState("");
-    // useNavigate return a function that we can use to navigate
+    
     const navigate = useNavigate();
-    //useEffect Hook is similar componentDidMount
+    
     useEffect(() =>
     {
-        //axios is a promised based web client
-        //make a HTTP Request with GET method and pass as part of the
-        //url.
+      
         axios.get('http://localhost:4000/api/book/' + id)
             .then((response) =>
             {
